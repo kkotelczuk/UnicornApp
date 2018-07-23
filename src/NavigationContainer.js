e831208d4dbfbb0166cfc85dd6602276e73ee58d
kkotelczuk <konrad.kotelczuk@gmail.com>
2018-07-23 07:38:24 +0200

    chore: cleanup

--- src/NavigationContainer.js
+++ src/NavigationContainer.js
@@ -2,20 +2,15 @@ import React from 'react';
 import {
   ActivityIndicator,
   AsyncStorage,
-  Button,
-  StatusBar,
-  StyleSheet,
   View,
 } from 'react-native';
-import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
 
 export default class AuthLoading extends React.Component {
-  constructor() {
-    super();
+  
+  componentDidMount() {
     this._bootstrapAsync();
   }
 
-  // Fetch the token from storage then navigate to our appropriate place
   _bootstrapAsync = async () => {
     const isAuthenticated = await AsyncStorage.getItem('@UnicornAppStore:user:isAuthenticated');
     const isRegistered = await AsyncStorage.getItem('@UnicornAppStore:user:name');
@@ -24,17 +19,12 @@ export default class AuthLoading extends React.Component {
     } else {
       this.props.navigation.navigate(isAuthenticated ? 'App' : 'Auth');
     }
-
-    // This will switch to the App screen or Auth screen and this loading
-    // screen will be unmounted and thrown away.
   };
 
-  // Render any loading content that you like here
   render() {
     return (
       <View>
         <ActivityIndicator />
-        <StatusBar barStyle="default" />
       </View>
     );
   }
